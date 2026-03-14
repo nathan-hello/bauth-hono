7335a8a83b27ebbe0eb11dea48fe76766775da9d
Nathan <natey3k@gmail.com>
2026-03-14 17:22:55 -0400

    moved stuff in public

--- src/views/components/layout.tsx
+++ src/views/components/layout.tsx
@@ -10,9 +10,9 @@ export function Layout({ meta, children, copy }: { meta: RouteMetadata; children
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-                <link rel="stylesheet" href="/styles.css" />
+                <link rel="stylesheet" href="/auth/styles.css" />
             </head>
-            <body class="font-sans bg-[url('/carpark.webp')] bg-center bg-cover bg-fixed min-h-screen p-4 flex items-center justify-center flex-col text-fg text-xl">
+            <body class="font-sans bg-[url('/images/carpark.webp')] bg-center bg-cover bg-fixed min-h-screen p-4 flex items-center justify-center flex-col text-fg text-xl">
                 <a href="/">
                     <img src="/favicon.svg" class="py-2 cursor-pointer mx-auto h-10 w-auto" />
                 </a>
